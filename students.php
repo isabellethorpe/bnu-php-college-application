ebcdeede9bd2e9c0b61d6dd231aa3f21a35a3b24
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-04-29 08:38:44 +0100

    fixed student bug

--- students.php
+++ students.php
@@ -1,36 +1,36 @@
 <?php
 
-include("_includes/config.inc");
-include("_includes/dbconnect.inc");
-include("_includes/functions.inc");
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
 
 
-// check logged in
-if (isset($_SESSION['id'])) {
+   // check logged in
+   if (isset($_SESSION['id'])) {
 
-  echo template("templates/partials/header.php");
-  echo template("templates/partials/nav.php");
+      echo template("templates/partials/header.php");
+      echo template("templates/partials/nav.php");
 
-  // Build SQL statment that selects all from STUDENT
-  $sql = "SELECT * FROM student";
-  $result = mysqli_query($conn, $sql);
+      // Build SQL statment that selects all from STUDENT
+      $sql = "SELECT * FROM student";
+      $result = mysqli_query($conn, $sql);
 
-  $data['content'] .= <<<EOD
+      $data['content'] .= <<<EOD
       <h2>Students</h2>
       <a href="addstudents.php" class="btn btn-primary mb-2">Add Student</a>
 
 EOD;
 
-  // form will POST to deletestudents.php
-  $data['content'] .= "<form action='deletestudents.php' method='post'>";
+                    // form will POST to deletestudents.php
+                    $data['content'] .= "<form action='deletestudents.php' method='post'>";
 
 
 
-  // prepare page content
-  $data['content'] .= "<table border='1'>";
+      // prepare page content
+      $data['content'] .= "<table border='1'>";
 
-
-  $data['content'] .= "<tr>
+     
+    $data['content'] .="<tr>
     <th>Student ID</th>
     <th>First Name</th>
     <th>Last Name</th>
@@ -45,39 +45,42 @@ EOD;
     <th>Edit</th>
     <th>Image</th>
     </tr>";
+      
+      // Display the student details within the html table
+      while($row = mysqli_fetch_array($result)) {
+        $data['content'] .="<tr>";
+        $data['content'] .= "<td> {$row["studentid"]} </td>";
+         $data['content'] .= "<td> {$row["firstname"]} </td>";
+         $data['content'] .= "<td> {$row["lastname"]} </td>";
+         //$data['content'] .= "<td> {$row["password"]} </td>";
+         $data['content'] .= "<td> {$row["dob"]} </td>";
+         $data['content'] .= "<td> {$row["house"]} </td>";
+         $data['content'] .= "<td> {$row["town"]} </td>";
+         $data['content'] .= "<td> {$row["county"]} </td>";
+         $data['content'] .= "<td> {$row["country"]} </td>";
+         $data['content'] .= "<td> {$row["postcode"]} </td>";
 
-  // Display the student details within the html table
-  while ($row = mysqli_fetch_array($result)) {
-    $data['content'] .= "<tr>";
-    $data['content'] .= "<td> {$row["studentid"]} </td>";
-    $data['content'] .= "<td> {$row["firstname"]} </td>";
-    $data['content'] .= "<td> {$row["lastname"]} </td>";
-    //$data['content'] .= "<td> {$row["password"]} </td>";
-    $data['content'] .= "<td> {$row["dob"]} </td>";
-    $data['content'] .= "<td> {$row["house"]} </td>";
-    $data['content'] .= "<td> {$row["town"]} </td>";
-    $data['content'] .= "<td> {$row["county"]} </td>";
-    $data['content'] .= "<td> {$row["country"]} </td>";
-    $data['content'] .= "<td> {$row["postcode"]} </td>";
-
-    // add values to each checkbox
-    $data['content'] .= "<td> <input type='checkbox' name='students[]'
+         // add values to each checkbox
+         $data['content'] .= "<td> <input type='checkbox' name='students[]'
           value='$row[studentid]'</td>";
-    $data['content'] .= "<td><a href='editstudent.php?id=" . $row['studentid'] . "'>Edit</a>";
-  }
-  $data['content'] .= "</table>";
+          $data['content'] .= "<td><a href='editstudent.php?id=".$row['studentid']."'>Edit</a>";
 
-  // delete button
-  $data['content'] .= "<br><br>";
 
-  $data['content'] .= "<input type='submit' class='deletebtn' name='deletebtn' value='Delete'/>";
+      }
+      $data['content'] .= "</table>";
 
-  $data['content'] .= "</form>";
+        // delete button
+      $data['content'] .="<br><br>";
 
-  // render the template
-  echo template("templates/default.php", $data);
-} else {
-  header("Location: index.php");
-}
+      $data['content'] .="<input type='submit' class='deletebtn' name='deletebtn' value='Delete'/>";
 
-echo template("templates/partials/footer.php");
+      $data['content'] .="</form>";
+
+      // render the template
+      echo template("templates/default.php", $data);
+
+   } else {
+      header("Location: index.php");
+   }
+
+   echo template("templates/partials/footer.php");
