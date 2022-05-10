61ca714fdf51a9421cb888a5de03ebe89a5d08f5
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-05-10 16:53:48 +0100

    created table displaying all student data

---
+++ students.php
@@ -0,0 +1,58 @@
+<?php
+
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
+
+
+   // check logged in
+   if (isset($_SESSION['id'])) {
+
+      echo template("templates/partials/header.php");
+      echo template("templates/partials/nav.php");
+
+      // Build SQL statment that selects all from STUDENT
+      $sql = "SELECT * FROM student";
+      
+        //  $sql = "select * from studentmodules sm, module m where m.modulecode = sm.modulecode and sm.studentid = '" . $_SESSION['id'] ."';";
+
+      $result = mysqli_query($conn,$sql);
+
+      // prepare page content
+      $data['content'] .= "<table border='1'>";
+     
+    $data['content'] .="<tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Date of birth</th>
+    <th>House</th>
+    <th>Town</th>
+    <th>County</th>
+    <th>Country</th>
+    <th>Postcode</th>
+    </tr>";
+      
+      // Display the student details within the html table
+      while($row = mysqli_fetch_array($result)) {
+        $data['content'] .="<tr>";
+         $data['content'] .= "<td> {$row["firstname"]} </td>";
+         $data['content'] .= "<td> {$row["lastname"]} </td>";
+         $data['content'] .= "<td> {$row["dob"]} </td>";
+         $data['content'] .= "<td> {$row["house"]} </td>";
+         $data['content'] .= "<td> {$row["town"]} </td>";
+         $data['content'] .= "<td> {$row["county"]} </td>";
+         $data['content'] .= "<td> {$row["country"]} </td>";
+         $data['content'] .= "<td> {$row["postcode"]} </td>";
+      }
+      $data['content'] .= "</table>";
+
+      // render the template
+      echo template("templates/default.php", $data);
+
+   } else {
+      header("Location: index.php");
+   }
+
+   echo template("templates/partials/footer.php");
+
+?>
