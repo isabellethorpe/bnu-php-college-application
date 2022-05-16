049f2eea13e49a5329ab3787d383190f29357240
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-05-16 11:22:22 +0100

    added bootstrap to log in form

--- templates/login.php
+++ templates/login.php
@@ -1,12 +1,20 @@
 
 <?php echo $message; ?>
 
+<br>
 <form name="frmLogin" action="authenticate.php" method="post">
+<div class="form-group">
    Student ID:
    <input name="txtid" type="text" />
+   </div>
    <br/>
+   <div class="form-group">
    Password:
    <input name="txtpwd" type="password" />
+   </div>
    <br/>
+
    <input type="submit" value="Login" name="btnlogin" />
+
 </form>
+
