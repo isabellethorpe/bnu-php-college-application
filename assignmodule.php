da4aa9f2590af2bd4fbd4f54bbdd041980491d3d
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-05-16 11:22:08 +0100

    edited page spacing

--- assignmodule.php
+++ assignmodule.php
@@ -25,13 +25,14 @@ if (isset($_SESSION['id'])) {
      $result = mysqli_query($conn, $sql);
 
      $data['content'] .= "<form name='frmassignmodule' action='' method='post' >";
-     $data['content'] .= "Select a module to assign<br/>";
+     $data['content'] .= "<br>Select a module to assign<br/><br>";
      $data['content'] .= "<select name='selmodule' >";
      // Display the module name sin a drop down selection box
      while($row = mysqli_fetch_array($result)) {
         $data['content'] .= "<option value='$row[modulecode]'>$row[name]</option>";
      }
      $data['content'] .= "</select><br/>";
+     $data['content'] .= "<br/>";
      $data['content'] .= "<input type='submit' name='confirm' value='Save' />";
      $data['content'] .= "</form>";
    }
