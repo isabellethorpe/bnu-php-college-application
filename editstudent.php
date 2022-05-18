037e790d759457e252439dc5f933d1aa6fce232e
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-05-18 13:17:21 +0100

    tidied up code section

--- editstudent.php
+++ editstudent.php
@@ -4,7 +4,6 @@ include("_includes/config.inc");
 include("_includes/dbconnect.inc");
 include("_includes/functions.inc");
 
-
 // check logged in
 if (isset($_SESSION['id'])) {
 
@@ -26,16 +25,6 @@ if (isset($_SESSION['id'])) {
    }
    else {
 
-    // studentid = studentid?
-
-
-
-   //  $sql = "SELECT image FROM student WHERE studentid='" . $_GET["studentid"] ."';";
-	
-   //  $result = mysqli_query($conn, $sql);
-   //  $row = mysqli_fetch_array($result);
-
-
       $sql = "SELECT * from student where studentid='". $_GET["studentid"] . "';";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result);
@@ -48,27 +37,46 @@ if (isset($_SESSION['id'])) {
       <h2>Edit Student</h2>
       <form name="formeditstudent" action="" method="post">
       <input type="hidden" name="id" value="{$row['studentid']}">
-
+      <div class="form-group">
       Student ID :
-      <input name="studentid" type="text" value="{$row['studentid']}" /><br/>
+      <input name="studentid" type="text" value="{$row['studentid']}" class="form-control" /><br/>
+      </div>
+      <div class="form-group">
       Password :
-      <input name="password" type="text" value="{$row['password']}" /><br/>
+      <input name="password" type="text" value="{$row['password']}" class="form-control"/><br/>
+      </div>
+      <div class="form-group">
       First Name :
-      <input name="firstname" type="text" value="{$row['firstname']}" /><br/>
+      <input name="firstname" type="text" value="{$row['firstname']}"class="form-control"/><br/>
+      </div>
+      <div class="form-group">
       Last Name :
-      <input name="lastname" type="text" value="{$row['lastname']}" /><br/>
+      <input name="lastname" type="text" value="{$row['lastname']}"class="form-control" /><br/>
+      </div>
+      <div class="form-group">
       Date of birth :
-      <input name="dob" type="text" value="{$row['dob']}" /><br/>
+      <input name="dob" type="text" value="{$row['dob']}"class="form-control" /><br/>
+      </div>
+      <div class="form-group">
       House :
-      <input name="house" type="text" value="{$row['house']}" /><br/>
+      <input name="house" type="text" value="{$row['house']}" class="form-control"/><br/>
+      </div>
+      <div class="form-group">
       Town :
-      <input name="town" type="text" value="{$row['town']}" /><br/>
+      <input name="town" type="text" value="{$row['town']}"class="form-control" /><br/>
+      </div>
+      <div class="form-group">
       County :
-      <input name="county" type="text" value="{$row['county']}" /><br/>
+      <input name="county" type="text" value="{$row['county']}"class="form-control" /><br/>
+      </div>
+      <div class="form-group">
       Country :
-      <input name="country" type="text" value="{$row['country']}" /><br/>
+      <input name="country" type="text" value="{$row['country']}"class="form-control" /><br/>
+      </div>
+      <div class="form-group">
       Postcode :
-      <input name="postcode" type="text" value="{$row['postcode']}" /><br/>
+      <input name="postcode" type="text" value="{$row['postcode']}"class="form-control" /><br/>
+      </div>
 
 
       <input type="submit" value="Save" name="submit"/>
