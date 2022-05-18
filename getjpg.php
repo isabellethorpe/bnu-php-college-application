0486f17c91b8d5624117999e3893305c2bd2c564
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-05-18 12:53:46 +0100

    added getjpg script

---
+++ getjpg.php
@@ -0,0 +1,19 @@
+17 lines (11 sloc)  358 Bytes
+   
+<?php
+
+  header("Content-type: image/jpeg");
+
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
+
+  $sql = "SELECT image FROM student WHERE studentid='" . $_GET["studentid"] ."';";
+	
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  
+  $jpg = $row["image"];
+
+  echo $jpg;
+?>
